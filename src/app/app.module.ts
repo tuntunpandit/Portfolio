407179569c01742e5645e7091fb486f332958396
tuntunpandit <tuntunkumarjamui@gmail.com>
2025-03-06 21:33:39 +0530

    restructed folder for user and admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { AboutComponent } from './components/about/about.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/User/hero/hero.component';
+import { AboutComponent } from './components/User/about/about.component';
+import { ProjectsComponent } from './components/User/projects/projects.component';
+import { ContactComponent } from './components/User/contact/contact.component';
+import { NavbarComponent } from './components/User/navbar/navbar.component';
+import { FooterComponent } from './components/User/footer/footer.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 @NgModule({
   declarations: [
