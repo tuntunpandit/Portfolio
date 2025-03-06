762e74527f4d850a7317f200822fb40c13a11837
tuntunpandit <tuntunkumarjamui@gmail.com>
2025-03-06 21:44:28 +0530

    Basic folder structure and routing added for admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/User/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./components/Admin/admin.module').then((m) => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './components/User/hero/hero.component';
-import { AboutComponent } from './components/User/about/about.component';
-import { ProjectsComponent } from './components/User/projects/projects.component';
-import { ContactComponent } from './components/User/contact/contact.component';
-import { NavbarComponent } from './components/User/navbar/navbar.component';
-import { FooterComponent } from './components/User/footer/footer.component';
+
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { AdminComponent } from './components/Admin/admin.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent,
-    NavbarComponent,
-    FooterComponent,
-  ],
+  declarations: [AppComponent, AdminComponent],
   imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule],
   providers: [],
   bootstrap: [AppComponent],
